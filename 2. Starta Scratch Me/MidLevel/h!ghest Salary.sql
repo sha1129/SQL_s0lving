dc105a811d5cbbf6cce5370a41107d080b8609ec
sha1129 <63379238+sha1129@users.noreply.github.com>
2022-11-30 21:08:49 -0500

    Update h!ghest Salary.sql

--- 2. Starta Scratch Me/MidLevel/h!ghest Salary.sql
+++ 2. Starta Scratch Me/MidLevel/h!ghest Salary.sql
@@ -1,6 +1,6 @@
-### F!nd th3 3mpl0y33 w!th th3   highest salary per department.
-### 0utput the department name, employee's first name along with the corresponding salary.
-### ID #9897
+-- F!nd th3 3mpl0y33 w!th th3   highest salary per department Nov30,2022
+-- 0utput the department name, employee's first name along with the corresponding salary.
+-- ID #9897
 
 WITH maxSalary AS (
   SELECT RANK() OVER (PARTITION BY department ORDER BY salary DESC) AS ranks, department, first_name, salary
