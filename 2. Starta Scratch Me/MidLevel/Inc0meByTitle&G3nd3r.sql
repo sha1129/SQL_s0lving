2e27515c4ad2a4aef2c7f3c55c4a4f6544cd2fd8
sha1129 <63379238+sha1129@users.noreply.github.com>
2022-11-30 21:05:26 -0500

    Create Inc0meByTitle&G3nd3r.sql

---
+++ 2. Starta Scratch Me/MidLevel/Inc0meByTitle&G3nd3r.sql
@@ -0,0 +1,22 @@
+### F!nd th3 @v3rag3 t0tal c0mpensation based 0n employee titles and gender
+### Two Tables sf_employee and sf_bonus
+
+### First c@lculate bonus and then calculate total compensation
+
+WITH sumBonus AS(
+    SELECT SUM(bonus) as bonus, worker_ref_id
+    FROM sf_bonus
+    GROUP BY 2
+),
+
+totalCompensation AS ( 
+    SELECT (b.bonus+e.salary) AS salaryTotal, 
+        e.employee_title as title, 
+        e.sex as gender
+    FROM sumBonus b
+    JOIN sf_employee e ON e.id = b.worker_ref_id
+)
+
+### Take average as Window function partition by Titles 
+SELECT DISTINCT title, gender, AVG(salaryTotal) OVER(PARTITION BY title)
+FROM totalCompensation
