4f572076b9c41d47859c360da8faff5fdde98dfe
sha1129 <63379238+sha1129@users.noreply.github.com>
2022-11-30 20:06:52 -0500

    Create h!ghest S@lary.sql

---
+++ 2. Starta Scratch Me/MidLevel/h!ghest S@lary.sql
@@ -0,0 +1,11 @@
+F!nd th3 3mpl0y33 w!th th3   highest salary per department.
+0utput the department name, employee's first name along with the corresponding salary.
+
+WITH maxSalary AS (
+  SELECT RANK() OVER (PARTITION BY department ORDER BY salary DESC) AS ranks, department, first_name, salary
+  FROM employee
+)
+
+SELECT first_name, department, salary 
+FROM maxSalary
+WHERE ranks = 1
